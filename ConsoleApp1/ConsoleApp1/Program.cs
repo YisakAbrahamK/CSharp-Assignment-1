edf0833a4a2de4ac922b2792def4da1b919811ef
YisakAbrahamK <107458880+YisakAbrahamK@users.noreply.github.com>
2022-06-14 16:30:24 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Repository test first line");
         }
     }
 }
